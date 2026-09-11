beb324167ef59d986c3d4516d28b0eef2cd2e067
Arun Shaw <arunshaw433@gmail.com>
2026-09-11 15:34:31 +0530

    Added

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const dialog = document.querySelector('dialog');
 
 dialog.onclick = (event) => {
-    if (event.target === event.currentTarget) {
-        event.target?.requestClose();
-    }
+    event.target?.requestClose();
 };
