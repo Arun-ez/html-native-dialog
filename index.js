940110f7873cdaa25e2621eb5e6ee84b68755dcf
Arun Shaw <arunshaw433@gmail.com>
2026-09-11 11:54:54 +0530

    Added fixes

--- index.js
+++ index.js
@@ -1,34 +1,7 @@
-
-// document.querySelector('dialog').onclick = (event) => {
-
-//     if (event.target === event.currentTarget) {
-
-//         const id = event.target?.id;
-
-//         const button = document.createElement('button');
-
-//         button.command = 'close';
-//         button.commandfor = id;
-
-//         button.click();
-//         console.log('Clicked on dialog');
-//     }
-// }
-
 const dialog = document.querySelector('dialog');
 
 dialog.onclick = (event) => {
     if (event.target === event.currentTarget) {
-        const id = event.target?.id;
-
-        const button = document.createElement('button');
-
-        button.setAttribute('command', 'close');
-        button.setAttribute('commandfor', id);
-
-
-        document.body.append(button);
-        button.click();
-        button.remove();
+        event.target?.requestClose();
     }
 };
